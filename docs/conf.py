064add69eb45eb07266bbcd1db6341149d90c1e2
Amit Saha <amitsaha.in@gmail.com>
2012-08-25 12:32:33 +1000

    mock module for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -259,3 +259,27 @@ epub_copyright = u'2010, Kenneth Reitz'
 
 # Allow duplicate toc entries.
 #epub_tocdup = True
+
+
+import sys
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        return Mock()
+
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return type(name, (), {})
+        else:
+            return Mock()
+
+MOCK_MODULES = ['args']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
